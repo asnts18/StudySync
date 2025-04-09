9ec70eb0907cfee94665d0b669b3ee5af7947336
Abegail Santos <abegailsantos.1819@gmail.com>
2025-04-08 23:39:51 -0400

    add TODO comments

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const courseController = require('../controllers/courseController');
 const auth = require('../middleware/auth');
 
+// TODO 
 // Public routes
 router.get('/university/:universityId', courseController.getCoursesByUniversity);
 
+// TODO 
 // Protected routes
 router.get('/my-courses', auth, courseController.getUserCourses);
 router.post('/enroll', auth, courseController.addUserToCourse);
--- server/routes/universityRoutes.js
+++ server/routes/universityRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const universityController = require('../controllers/universityController');
 
+// TODO
 router.get('/', universityController.getAllUniversities);
 router.get('/:id', universityController.getUniversityById);
 
